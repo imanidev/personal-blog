936855fcf389b08db0de762a8e41d7d3e241dd54
imani.devs <imaniroberts.dev@gmail.com>
2023-06-05 14:58:42 -0400

    added db connection

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", function (error) {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
